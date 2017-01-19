5a5a64b0748edc6af2e8db14846d319cff76a2f6
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-20 10:55:15 +1300

    Created UI

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var trafficLight: UIImageView!
+    
+    @IBOutlet var counterLabel: UILabel!
+    
+    @IBOutlet var btnStartStop_clicked: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   
+    @IBAction func StartStop(_ sender: Any) {
+    }
 
 }
 
